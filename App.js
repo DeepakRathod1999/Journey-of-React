1c00ebee22b089bc9a68f3fcecfebb2bbbb63793
Deepak Rathod <deepakrathod590@gmail.com>
2024-09-06 12:32:35 +0530

    stage 2 chapter 2

--- App.js
+++ App.js
@@ -1,4 +1,6 @@
-
+import React from "react";
+import  ReactDOM  from "react-dom";
 const heading = React.createElement("h1",{id:"child1"},"i am the heading");
-const root= ReactDOM.createRoot(document.getElementById("root"));
+const bridge=document.getElementById("root");
+const root= ReactDOM.createRoot(bridge);
 root.render(heading);
