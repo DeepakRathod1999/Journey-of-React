f6c25da4b531967de963d307f15f0641ac00fed5
Deepak Rathod <deepakrathod590@gmail.com>
2024-09-26 15:47:45 +0530

    stage 3 chapter 3

--- App.js
+++ App.js
@@ -1,6 +1,21 @@
 import React from "react";
 import  ReactDOM  from "react-dom";
-const heading = React.createElement("h1",{id:"child1"},"i am the heading");
+// const heading = React.createElement("h1",{id:"child1"},"i am the heading");
 const bridge=document.getElementById("root");
 const root= ReactDOM.createRoot(bridge);
-root.render(heading);
+
+const App=()=>{
+    const title =(
+<div>
+    <h1>Hello Demo </h1>
+    <h2>This is Amazing</h2>
+
+</div>    );
+    return (
+        <div>
+        <h1>This is Arrow Way</h1>
+        {title}    
+        </div>
+        )
+}
+root.render(<App/>);
